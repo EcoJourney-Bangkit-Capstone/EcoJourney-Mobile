b66add742fcdead9cf0eb1c1fe304eebd1e1d042
Fadlan Ariel <fadlan.ariel@ui.ac.id>
2024-06-15 08:39:20 +0700

    feat: Implement splash screen

---
+++ app/src/main/java/com/bangkit/ecojourney/ui/SplashScreenActivity.kt
@@ -0,0 +1,34 @@
+package com.bangkit.ecojourney.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.bangkit.ecojourney.MainActivity
+import com.bangkit.ecojourney.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_SCREEN_DURATION)
+    }
+
+    companion object {
+        private const val SPLASH_SCREEN_DURATION = 3000L
+    }
+}
